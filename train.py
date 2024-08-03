15f2162357a57dbed44647de0c816f266d5d21f2
Stefano Segnini <34490822+stefano2211@users.noreply.github.com>
2024-08-02 21:15:54 -0400

    update

--- train.py
+++ train.py
@@ -14,11 +14,8 @@ from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
-
-
-
 from model import build_transformer
-from dataset import BilingualDataset, causal_mask
+from datasets import BilingualDataset, causal_mask
 from config import get_config, get_weights_file_path, latest_weights_file_path
 
 import torch
